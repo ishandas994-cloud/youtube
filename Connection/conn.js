f57a5d60f0dfa86b56a20cdfdaae0f4c68e93e75
ishandas994-cloud <ishandas994@gmail.com>
2026-03-15 23:33:57 +0530

    test

--- Connection/conn.js
+++ Connection/conn.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 const seedDatabase = require("./seed");
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-  console.log("MongoDB Connected ✅");
-})
-.catch((err) => console.log("Mongo Error:", err));
+const mongoUri = process.env.MONGO_URL;
+if (!mongoUri) {
+  console.error("MongoDB connection URI is missing. Please set MONGO_URL in .env.");
+  process.exit(1);
+}
+
+console.log("Connecting to MongoDB at:", mongoUri);
+
+mongoose
+  .connect(mongoUri)
+  .then(() => {
+    console.log("MongoDB Connected ✅");
+  })
+  .catch((err) => console.log("Mongo Error:", err));
